3613c791160c569c8d8f9b6ad350c95653633cbf
Brian Parbhu <brian.parbhu@gmail.com>
2023-06-26 18:07:30 -0400

    adding script to run all tests

---
+++ test/run_tests.jl
@@ -0,0 +1,13 @@
+using Loo
+using Test
+
+include("function_method_stuff.jl")
+include("test_0_helpers.jl")
+include("test_compare.jl")
+include("test_crps.jl")
+include("test_E_loo.jl")
+include("test_extract_log_lik.jl")
+include("test_gpdfit.jl")
+include("test_kfold_helpers.jl")
+include("test_loo_and_waic.jl")
+include("test_loo_approximate_posterior.jl")
